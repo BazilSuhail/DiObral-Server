2d67e453a355c1255839dde99252623128df3437
BazilSuhail <bazil1854@gmail.com>
2025-06-09 02:30:10 +0500

    Update Order Schema to include product's image

--- models/order.js
+++ models/order.js
@@ -4,6 +4,7 @@ const orderItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     discountedPrice: { type: Number, required: true },
+    image: { type: Number, required: true },
     quantity: { type: Number, required: true },
     size: { type: String, required: true }
 });
